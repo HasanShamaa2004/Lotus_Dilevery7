34cdd6a077ba333371b941d32c0e272bf7de7ce4
Hasan <134393657+HasanShamaa2004@users.noreply.github.com>
2023-12-28 21:33:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+const translations = {
+    en: {
+        services: 'Services',
+        about: 'About',
+        contact: 'Contact Us',
+        english: 'English',
+        arabic: 'Arabic',
+        hellothere: 'welcome',
+        wewerepleased:
+            'We were happy to visit our company, the company, the lotus flower',
+        tellus: 'Tell us about your thoughts',
+        welcometo:
+            'You are welcome at the connection company, the lotus flower, which offers the best delivery and the fastest time',
+        wewilld: 'We will connect all you want',
+        wearepleased:
+            'We are pleased to visit our site, use our services, and deal with us, thank you for your trust',
+        yourproduct: 'We meet all your needs',
+        welocometo:
+            'You are welcome in our company to pay all your needs and be the best in the best condition',
+        instant: 'VIP Delivery Service',
+        instantd:
+            'Your request was large or small within an hour to arrive in the Emirates.We are Al Lotus Company to deliver requests within the Emirates, we found the ideal solution that meets your need for speeding your request through our efforts and training for drivers to save the roads and bridges of the state, and to pay attention to air -conditioned, refrigerated delivery cars dedicated to safe transport.',
+        delivof: "Connecting companies' requests",
+        delivall:
+            'Companies are considered one of the most demanding places to deliver requests and they need a reliable and licensed delivery company in the Emirates, so Al Lotus Company has been connected to requests a specialized team to work to meet companies within a deliberate plan to raise the capacity of companies and their requirements for the speed of safe delivery.',
+        foodd: 'Delivery of private requests',
+        delivfood:
+            'The actual solution in delivering requests, especially is to cooperate with an internal transportation company in the Emirates, which is characterized by providing cheap and fair prices, in addition to providing excellent delivery services',
+        delivanimal: 'Delivery of home requests',
+        delivpet:
+            "The lotus company connects the requests to facilitate the process of transporting goods and ease of delivery within the country, which is concerned with the details of the demand in terms of quality and size accurately, with legal contracts signed to preserve the product and ensure the receipt and delivery rights in the time and place, where quality, experience and development of the company's efforts through trainingDrivers on how to deliver household goods in the Emirates",
+        welcomrour:
+            'Welcome to Lotus Flower Company. We are here to meet all your needs and supplies are ready at all times to deliver all orders as quickly as possible. We hope that you will benefit from our services',
+        feelfr: ': Do not hesitate to leave a plan for us on our e -mail',
+        tocontact: ': To contact our number',
+        findus: 'You find us on social networks',
+        flower: 'Lotus flower company',
+    },
+    ar: {
+        services: 'خدمات',
+        about: 'المزيد عنا',
+        contact: 'للتواصل معنا',
+        // selectLanguage: "إختر لغة",
+        english: 'الانجليزية',
+        arabic: 'العربية',
+        hellothere: 'أهلا وسهلا بكم',
+        wewerepleased: 'سعدنا بزيارتك لشركتنا شركة التوصيل زهرة اللوتس',
+        tellus: 'أخبرنا عن أفكارك',
+        welcometo:
+            'اهلا وسهلا بكم في شركة التوصيل زهرة اللوتس التي تقدم افضل توصيل وباسرع وقت',
+        wewilld: 'سوف نقوم بتوصيل كل ماتريد',
+        wearepleased:
+            'لنا السرور بزيارتك لموقعنا واستخدام خدماتنا والتعامل معنا شكرا لثقتكم',
+        yourproduct: 'نقوم بتلبية جميع احتياجاتك',
+        welocometo:
+            'اهلا وسهلا بكم في شركتنا لتبية جميع احتياجاتكن وتكونو بأفضل حال',
+        instant: 'vip خدمة توصيل',
+        instantd:
+            ' طلبك كبير كان او صغير خلال ساعة بيوصل في الامارات. نحن شركة اللوتس لتوصيل الطلبات داخل الامارات , وجدنا الحل الامثل الذي يلبي حاجتك بالسرعة في توصيل طلبك من خلال جهودنا وتدريبنا للسائقين لحفظ طرقات وجسور الدولة , والاهتمام بسيارات توصيل مكيفة ومبردة ومخصصة للنقل الآمن.',
+        delivof: 'توصيل طلبات الشركات',
+        delivall:
+            'يعتبر الشركات من اكثر الاماكن طلباً لتوصيل الطلبات ويحتاجون لشركة توصيل موثوقة ومرخصة في الامارات, لذلك قامت شركة اللوتس لتوصيل الطلبات بتدريب فريق مختص للعمل على تلبية الشركات ضمن خطة مدروسة لرفع القدرة الاستيعابية للشركات ومتطلباتهم لسرعة التوصيل الآمن.',
+        foodd: 'توصيل الطلبات الخاصة',
+        delivfood:
+            'الحل الفعلي في توصيل الطلبات خاصة هو التعاون مع إحدى شركات النقل الداخلي في الأمارات والتي تتميز بتقديم أسعار رخيصة وعادلة، بالإضافة إلى تقديم خدمات توصيل ممتازة',
+        delivanimal: 'توصيل طلبات المنازل',
+        delivpet:
+            ' تقوم شركة اللوتس لتوصيل الطلبات بتسهيل عملية نقل البضائع وسهولة التوصيل داخل الدولة, تهتم بتفاصيل الطلب من حيث النوعية و الحجم بشكل دقيق, مع توقيع عقود قانونية للحفاظ على المنتج وضمان حقوق الاستلام والتسليم في الوقت والمكان المحدد,حيث الجودة والخبرة وتطوير جهود الشركة من خلال تدريب السائقين على كيفية تسليم البضائع المنزلية في الامارت',
+        welcomrour:
+            'اهلا وسهلا بكم في شركة التوصيل زهرة اللوتس نحن هنا لتلبية جميع احتياجاتكن ومستلزماتكن جاهزون في كل الأوقات لتوصيل جمبع الطلبات في اسرع وقت نامل الاستفادة من خدماتنا',
+        feelfr: ': لا تتردد في ترك لنا خطة على ايميلنا',
+        tocontact: ': للتواصل على رقمنا',
+        findus: 'تجدنا على الشبكات الاجتماعية',
+        flower: 'شركة زهرة اللوتس',
+    },
+}
+
+const languageSelector = document.querySelector('select')
+languageSelector.addEventListener('change', (event) => {
+    setLanguage(event.target.value)
+    // localStorage.setItem("lang", event.target.value);
+    setItem('lang', event.target.value)
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const language = getItem('lang') || 'en' // اذا لم تكن اللغة متوفرة استخدم الانجليزية
+    setLanguage(language)
+})
+
+const setLanguage = (language) => {
+    const elements = document.querySelectorAll('[data-i18n]')
+    elements.forEach((element) => {
+        const translationKey = element.getAttribute('data-i18n')
+        element.textContent = translations[language][translationKey]
+    })
+    // document.dir = language === 'ar' ? 'rtl' : 'ltr'
+}
